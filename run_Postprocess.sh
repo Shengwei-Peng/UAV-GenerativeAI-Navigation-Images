1279ba8993d5c4143a5f34f76c6765c492bb1981
ken <m11207330@mail.ntust.edu.tw>
2024-05-31 19:33:33 +0800

    update

--- run_Postprocess.sh
+++ run_Postprocess.sh
@@ -1,5 +1,5 @@
 echo "Postprocessing results..."
 python postprocess.py \
-  --diffusion_results_folder="./results/diffusion/HR" \
+  --diffusion_results_folder="./results/diffusion" \
   --gan_results_folder="./results/gan" \
   --output_folder="./submission"
