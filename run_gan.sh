c5af6423a05fbfeaaeb50066e497c89c3631e76d
ken <m11207330@mail.ntust.edu.tw>
2024-05-31 23:24:25 +0800

    update

--- run_gan.sh
+++ run_gan.sh
@@ -1,46 +1,46 @@
 echo "[GAN] Preprocessing data..."
-python ./GAN/preprocess.py \
+python ./gan/preprocess.py \
   --train_data_folder="./training_dataset" \
   --test_data_folder="./testing_dataset" \
-  --output_folder="./GAN/datasets"
+  --output_folder="./gan/datasets"
 
 
 echo "[GAN] Training for river data..."
-python ./GAN/train.py \
+python ./gan/train.py \
   --seed=0 \
   --model=pix2pix \
   --direction=AtoB \
   --netG=unet_256 \
   --ngf=256 \
   --ndf=256 \
-  --dataroot="./GAN/datasets/RI" \
+  --dataroot="./gan/datasets/RI" \
   --name="gan/RI" \
   --batch_size=32 \
-  --n_epochs=200 \
-  --n_epochs_decay=200
+  --n_epochs=1 \
+  --n_epochs_decay=1
 
 
 echo "[GAN] Training for road data..."
-python ./GAN/train.py \
+python ./gan/train.py \
   --seed=0 \
   --model=pix2pix \
   --direction=AtoB \
   --netG=unet_256 \
   --ngf=256 \
   --ndf=256 \
-  --dataroot="./GAN/datasets/RO" \
+  --dataroot="./gan/datasets/RO" \
   --name="gan/RO" \
   --batch_size=32 \
-  --n_epochs=200 \
-  --n_epochs_decay=200
+  --n_epochs=1 \
+  --n_epochs_decay=1
 
 
 echo "[GAN] Testining for river data..."
-python ./GAN/test.py \
+python ./gan/test.py \
   --seed=0 \
   --model=pix2pix \
   --direction=AtoB \
-  --dataroot="./GAN/datasets/RI" \
+  --dataroot="./gan/datasets/RI" \
   --netG=unet_256 \
   --ngf=256 \
   --name=gan/RI
--- run_router.sh
+++ run_router.sh
@@ -1,5 +1,5 @@
-echo "Postprocessing results..."
-python postprocess.py \
+echo "[Router] Selecting from the results..."
+python ./router/router.py \
   --diffusion_results_folder="./results/diffusion" \
   --gan_results_folder="./results/gan" \
   --output_folder="./submission"
